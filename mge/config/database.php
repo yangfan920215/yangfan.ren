42aef07956640fbcb93ce3540c39193363578172
yangfan <yangfan.ren@foxmail.com>
2017-07-03 23:09:26 +0800

    x

--- mge/config/database.php
+++ mge/config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mge_yangfan_app'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'fengyang920213'),
+            'password' => env('DB_PASSWORD', 'fengyang920213A'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
