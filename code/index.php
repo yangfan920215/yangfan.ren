2c4822a0628ddb5c4f25e36cab596d2b6f7e6e6e
phoenix <yangfan.ren@foxmail.com>
2017-08-22 18:04:17 +0800

    a

--- code/index.php
+++ code/index.php
@@ -1,5 +1,4 @@
 <?php
-phpinfo();
 /**
  * 入口文件
  * @author yangfan.ren@foxmail.com
@@ -42,8 +41,7 @@ require('./vendor/autoload.php');
 <title>code.yangfan.ren</title>
 </head>
 <body>
-
-<DIV align=center><IFRAME src="https://avgle.com/embed/7b35e6eb462bbf80915c" frameBorder=0 marginwidth=0 marginheight=0 scrolling=no style="width:1500px;height:800px;" width=168  height=50 scrolling=no ALLOWTRANSPARENCY="true"></IFRAME></DIV>
+<video id="videoPlayer" controls="" poster="http://i1.letvimg.com/lc10_yunzhuanma/201708/05/03/52/e54399040076011bbe21600e1092ac3f_v2_NDgzNzE2NDE2/thumb/2_640_360.jpg" autoplay="true" src="http://play.g3proxy.lecloud.com/vod/v2/MjYwLzIvODAvYmNsb3VkLzEwMDAwMS92ZXJfMDBfMjItMTExMTk3MjE2NC1hdmMtNDE5NjU1LWFhYy00ODAwMC02OTg5Njk5LTQxOTQyMTAwNi0yMTNiNDlhODMwNDNjYzZlMDcwMzgyNzBhMzExMzExZS0xNTAxODg1Nzg4NDU1Lm1wNA==?b=479&amp;mmsid=241857708&amp;tm=1503380670&amp;key=817acf1dc17401586989a3074e097e00&amp;platid=2&amp;splatid=209&amp;playid=0&amp;tss=ios&amp;vtype=13&amp;cvid=0&amp;payff=0&amp;uuid=70c74ab2&amp;pip=78f3f5e858034bf6b60d7dd4a866e69d&amp;tag=macos&amp;sign=bcloud_100001&amp;termid=2&amp;pay=0&amp;ostype=macos&amp;hwtype=un" style="width: 100%; height: 100%;"></video>
 <script type="text/javascript">
 </script>
 </body>
